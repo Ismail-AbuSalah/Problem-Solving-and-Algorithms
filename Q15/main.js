24fcd4a5a50946d5649ea1bc54d1eb24701fa50f
Ismail-AbuSalah <ismail.i.abusalah@gmail.com>
2020-12-11 16:37:30 +0200

    solved

--- Q15/main.js
+++ Q15/main.js
@@ -1,4 +1,4 @@
-console.log('Problem Solving Q:15 ');
+console.log("Problem Solving Q:15 ");
 
 /*  commonCharacters*/
 
@@ -8,10 +8,20 @@ Your function should return the common characters in the same order that they ap
 Do not return duplicate characters and ignore whitespace in your returned string.
 */
 
-function commonCharacters() {
-  // YOUR CODE HERE
+function commonCharacters(str1, str2) {
+  const arr1 = str1.split("");
+  const arr2 = str2.split("");
+  console.log(arr1.filter((value) => arr2.includes(value)));
+  // console.log(arr1.filter((value) => arr2.includes(value)));
+  return arr1
+    .filter((value) => arr2.includes(value))
+    .filter((item, i, arr) => {
+      console.log(i,item, arr.indexOf(item));
+      return (arr.indexOf(item) == i && item !== " ");
+    })
+    .join("");
 }
-
+// .join("")
 /* 
 Examples:
 commonCharacters('abc', 'abc') // => 'abc'
